d8be8aa1eb5ca3c439fc0b283db2d05e0ff12343
M1000 <50182658+M1O0O@users.noreply.github.com>
2022-01-17 23:18:19 +0100

    🐛 Useless now with ArgsType

--- src/Commands/say.ts
+++ src/Commands/say.ts
@@ -10,6 +10,6 @@ export const command: Command = {
     usage: '<member> <message>',
     argsType: ['member', 'longstring'],
     run: async (client, message, args) => {
-        message.channel.send(args.join(' '));
+        message.channel.send(args);
     }
 };
