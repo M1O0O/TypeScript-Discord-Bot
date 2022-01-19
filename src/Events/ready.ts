61ea7c5dfe67f1938c7eca5ead4780e385d24c91
M1000 <formentel.remi@gmail.com>
2022-01-19 14:03:43 +0100

    😎 Too dark for me 😆

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -3,6 +3,6 @@ import { Event } from "../Interfaces";
 export const event: Event = {
     name: "ready",
     run: (client) => {
-        client.prompt.print.success(`Logged in as ${client.prompt.Colors.Black}${client.user.tag}!`);
+        client.prompt.print.success(`Logged in as ${client.prompt.Colors.Green}${client.user.tag}!`);
     }
 }
